057c6d73130caff40f8c5c55ab4586f7c545bfdb
ahmad <ahmad.afandi85@gmail.com>
2021-05-08 05:58:48 +0700

    add functionality to send attachmet from public url (internet)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const fs = require('fs');
+const Config = require('./config.json')
+const Kurir = require('./kurir')
+const fs = require('fs')
+const qrcode = require('qrcode-terminal')
+const Downloader = require('./downloader')
+const WebSocket = require('ws')
 const {
-    Client,
-    Location,
+    Client,    
     MessageMedia,
 } = require('whatsapp-web.js');
-
+const downloadManager = new Downloader()
+let kurir
+let filePath
 const SESSION_FILE_PATH = './session.json';
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
@@ -12,9 +18,7 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
 }
 
 const client = new Client({
-    puppeteer: {
-        headless: false
-    },
+    puppeteer: Config.puppeteer,
     session: sessionCfg
 });
 // You can use an existing session and avoid scanning a QR code by adding a "session" object to the client options.
@@ -24,7 +28,11 @@ client.initialize();
 
 client.on('qr', (qr) => {
     // NOTE: This event will not be fired if a session is specified.
-    console.log('QR RECEIVED', qr);
+    console.log('QR RECEIVED', qr)
+    if (Config.puppeteer.headless) {
+        qrcode.generate(qr, {small: true})
+    }
+    
 });
 
 client.on('authenticated', (session) => {
@@ -44,160 +52,15 @@ client.on('auth_failure', msg => {
 
 client.on('ready', () => {
     console.log('READY');
+    kurir = new Kurir(client)
 });
 
 client.on('message', async msg => {
-    console.log('MESSAGE RECEIVED', msg);
-
-    if (msg.body == '!ping reply') {
-        // Send a new message as a reply to the current one
-        msg.reply('pong');
-
-    } else if (msg.body == '!ping') {
+    console.log(msg)
+    if(kurir === undefined) return
+    if (msg.body.startsWith('/')) {
         // Send a new message to the same chat
-        client.sendMessage(msg.from, 'pong');
-
-    } else if (msg.body.startsWith('!sendto ')) {
-        // Direct send a new message to specific id
-        let number = msg.body.split(' ')[1];
-        let messageIndex = msg.body.indexOf(number) + number.length;
-        let message = msg.body.slice(messageIndex, msg.body.length);
-        number = number.includes('@c.us') ? number : `${number}@c.us`;
-        let chat = await msg.getChat();
-        chat.sendSeen();
-        client.sendMessage(number, message);
-
-    } else if (msg.body.startsWith('!subject ')) {
-        // Change the group subject
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            let newSubject = msg.body.slice(9);
-            chat.setSubject(newSubject);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body.startsWith('!echo ')) {
-        // Replies with the same message
-        msg.reply(msg.body.slice(6));
-    } else if (msg.body.startsWith('!desc ')) {
-        // Change the group description
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            let newDescription = msg.body.slice(6);
-            chat.setDescription(newDescription);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body == '!leave') {
-        // Leave the group
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            chat.leave();
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body.startsWith('!join ')) {
-        const inviteCode = msg.body.split(' ')[1];
-        try {
-            await client.acceptInvite(inviteCode);
-            msg.reply('Joined the group!');
-        } catch (e) {
-            msg.reply('That invite code seems to be invalid.');
-        }
-    } else if (msg.body == '!groupinfo') {
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            msg.reply(`
-                *Group Details*
-                Name: ${chat.name}
-                Description: ${chat.description}
-                Created At: ${chat.createdAt.toString()}
-                Created By: ${chat.owner.user}
-                Participant count: ${chat.participants.length}
-            `);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body == '!chats') {
-        const chats = await client.getChats();
-        client.sendMessage(msg.from, `The bot has ${chats.length} chats open.`);
-    } else if (msg.body == '!info') {
-        let info = client.info;
-        client.sendMessage(msg.from, `
-            *Connection info*
-            User name: ${info.pushname}
-            My number: ${info.me.user}
-            Platform: ${info.platform}
-            WhatsApp version: ${info.phone.wa_version}
-        `);
-    } else if (msg.body == '!mediainfo' && msg.hasMedia) {
-        const attachmentData = await msg.downloadMedia();
-        msg.reply(`
-            *Media info*
-            MimeType: ${attachmentData.mimetype}
-            Filename: ${attachmentData.filename}
-            Data (length): ${attachmentData.data.length}
-        `);
-    } else if (msg.body == '!quoteinfo' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-
-        quotedMsg.reply(`
-            ID: ${quotedMsg.id._serialized}
-            Type: ${quotedMsg.type}
-            Author: ${quotedMsg.author || quotedMsg.from}
-            Timestamp: ${quotedMsg.timestamp}
-            Has Media? ${quotedMsg.hasMedia}
-        `);
-    } else if (msg.body == '!resendmedia' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-        if (quotedMsg.hasMedia) {
-            const attachmentData = await quotedMsg.downloadMedia();
-            client.sendMessage(msg.from, attachmentData, {
-                caption: 'Here\'s your requested media.'
-            });
-        }
-    } else if (msg.body == '!location') {
-        msg.reply(new Location(37.422, -122.084, 'Googleplex\nGoogle Headquarters'));
-    } else if (msg.location) {
-        msg.reply(msg.location);
-    } else if (msg.body.startsWith('!status ')) {
-        const newStatus = msg.body.split(' ')[1];
-        await client.setStatus(newStatus);
-        msg.reply(`Status was updated to *${newStatus}*`);
-    } else if (msg.body == '!mention') {
-        const contact = await msg.getContact();
-        const chat = await msg.getChat();
-        chat.sendMessage(`Hi @${contact.number}!`, {
-            mentions: [contact]
-        });
-    } else if (msg.body == '!delete' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-        if (quotedMsg.fromMe) {
-            quotedMsg.delete(true);
-        } else {
-            msg.reply('I can only delete my own messages');
-        }
-    } else if (msg.body === '!archive') {
-        const chat = await msg.getChat();
-        chat.archive();
-    } else if (msg.body === '!mute') {
-        const chat = await msg.getChat();
-        // mute the chat for 20 seconds
-        const unmuteDate = new Date();
-        unmuteDate.setSeconds(unmuteDate.getSeconds() + 20);
-        await chat.mute(unmuteDate);
-    } else if (msg.body === '!typing') {
-        const chat = await msg.getChat();
-        // simulates typing in the chat
-        chat.sendStateTyping();
-    } else if (msg.body === '!recording') {
-        const chat = await msg.getChat();
-        // simulates recording audio in the chat
-        chat.sendStateRecording();
-    } else if (msg.body === '!clearstate') {
-        const chat = await msg.getChat();
-        // stops typing or recording in the chat
-        chat.clearState();
+        kurir.chat(msg);
     }
 });
 
@@ -267,46 +130,46 @@ client.on('disconnected', (reason) => {
     console.log('Client was logged out', reason);
 });
 
-const WebSocket = require('ws')
-
 const wss = new WebSocket.Server({
-    port: 8888
+    port: Config.websocketPort
 })
 
 wss.on('connection', ws => {
-    ws.on('message', message => {
-        console.log(`Received message => ${message}`)
+    ws.on('message', async message => {
+        //console.log(`Received message => ${message}`)
         handleMessage(message);
     })
 })
 
-function handleMessage(e) {
+async function handleMessage(e) {
     let obj = JSON.parse(e);
     if (obj.type == 'sendWa') {
         //  let validType = ['text', 'image', 'document', 'location', 'video']
         let numbers = obj.data
         let options = {}
-        for (var i in numbers) {
+        for (var i in numbers) {            
             options = numbers[i].options !== undefined ? numbers[i].options : {}
+            if (options.media) {
+                options.media = new MessageMedia(options.media.mimetype, options.media.b64data, options.media.filename)
+            }
+            
+            if (numbers[i].url_public) {
+                filePath = Config.folderDownload +"/"+ numbers[i].url_public.substring(numbers[i].url_public.lastIndexOf('/') + 1);
+                await downloadManager.download(numbers[i].url_public,filePath)
+                        .then(fileInfo => numbers[i].attachment = fileInfo.path)
+                        .catch(err => console.log(err))
+            }
+
             if (numbers[i].attachment !== undefined) {
                 if (numbers[i].attachment) {
                     if (fs.existsSync(numbers[i].attachment)) {
                         options['media'] = MessageMedia.fromFilePath(numbers[i].attachment)
                     }
-                }
+                }                         
             }
-            // if (options.media) {
-            //     options.media = new MessageMedia(options.media.mimetype, options.media.b64data, options.media.filename)
-            // }
+            
             client.sendMessage(numbers[i].to, numbers[i].message, options);
         }
     }
 }
 
-function broadcast(data) {
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
-}
---
+++ readme.md
@@ -0,0 +1,27 @@
+# wa-broadcast
+
+## What is wa-broadcast?
+
+wa-broadcast is simple application to send bulk whatsapp message using whatsapp-web.js
+
+## Installation & setup
+
+- clone this repository `git clone https://github.com/pandigresik/wa-broadcast.git`
+- `cd wa-broadcast`
+- `npm install` to install dependency this application
+- change your `path of google chrome` key in `config.json` on line 6 variable `executablePath`
+- `npm run start` to run this application on your terminal / console
+- if you first run this application, you must scan qr code whatsapp to login
+- if you want to send bulk message open send.html in your browser and you can drag your spreadsheet file ( message.xlsx in my example file )
+
+## Feature
+
+- Send attachment from local disk or internet
+
+## Copyright
+
+wa-broadcast dikembangkan dan dimaintain oleh [asligresik](https://github.com/pandigresik)
+
+## Lisensi
+
+Lisensi dari wa-broadcast adalah [MIT License](LICENSE) namun proyek yang dibangun menyeseuaikan dengan kebijakan masing-masing
